bd2b2d90a3eceac63cdb3b810ce1f5b5c2759640
Flamur-Maliqi <flamur.maliqi@ibm.com>
2025-02-01 18:55:59 +0100

    Resume preview

--- src/components/ResumeComponent/ResumeComponent.jsx
+++ src/components/ResumeComponent/ResumeComponent.jsx
@@ -1,12 +1,19 @@
-import "./ResumeComponent.css"
-
+import "./ResumeComponent.css";
 
 function ResumeComponent() {
-    return (
-        <div>
-            <h1 className="resume-header">Resume</h1>
-        </div>
-    )
+  return (
+    <div className="resume">
+      <h1 className="resume-header">Resume</h1>
+      <div className="resume-container">
+        <iframe
+          src="../../../public/Resume.pdf"
+          width="80%"
+          height="800px"
+          title="Lebenslauf Vorschau"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default ResumeComponent;
