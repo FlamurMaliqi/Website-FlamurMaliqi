6d2c6778a170e82b7f8e2763c326c387cdd035f9
Flamur-Maliqi <flamur.maliqi@ibm.com>
2025-02-02 08:04:24 +0100

    project ein bisschen weitergemacht

---
+++ src/components/ProjectComponent/ProjectComponent.jsx
@@ -0,0 +1,11 @@
+import "./ProjectComponent.css"
+
+function ProjectComponent() {
+    return (
+        <div className="project-container">
+
+        </div>
+    )
+}
+
+export default ProjectComponent;
--- src/components/ProjectsComponent/ProjectsComponent.jsx
+++ src/components/ProjectsComponent/ProjectsComponent.jsx
@@ -1,10 +1,17 @@
 import "./ProjectsComponent.css"
+import ProjectComponent from "../ProjectComponent/ProjectComponent.jsx"
 
 
 function ProjectsComponent() {
     return (
         <div>
             <h1 className="projects-header">Projects</h1>
+            <hr className="line"></hr>
+            <div className="projects-container">
+                <ProjectComponent/>
+                <ProjectComponent/>
+                <ProjectComponent/>
+            </div>
         </div>
     )
 }
